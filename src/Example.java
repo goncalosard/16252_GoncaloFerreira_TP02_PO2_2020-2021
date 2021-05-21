1bec1c1489c35120a1f7514389a2174ac6f9ff65
16252-po2 <16252@stu.ipbeja.pt>
2021-05-21 22:04:22 +0100

    Test

---
+++ src/Example.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Gonçalo Sardinha Alves Ferreira 16252
+ * @version 21/05/2021
+ */
+public class Example extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
